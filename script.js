a8f7dfd2b2c6f842e04510bae2f8ea7cfff503f4
elibrowne <eli.browne.23@bishops.com>
2021-01-13 10:18:45 -0800

    Fixed JSON issues; it seems to save correctly now

--- script.js
+++ script.js
@@ -21,40 +21,23 @@ function storeData() {
   // Now, replace the window with the new data
   display(userResponses, true); // true is to replace, false is to append
   // Add to the localStorage data
-  var tempList = localStorage.getItem('inputs');
-  console.log(tempList);
-  if (typeof tempList == 'array') {
-    console.log("tempList existed");
-    tempList.push(userResponses);
-  } else {
-    console.log("tempList wasn't an array, making new array");
-    tempList = new Array();
-    tempList.push([userResponses]);
+  var tempList = localStorage.getItem('data');
+  // If the data is empty, change it to a JSON object before adding to it.
+  if (tempList == null || tempList == "{}") {
+    tempList = "{\"data\":[]}";
   }
+  console.log(tempList);
+  tempList = JSON.parse(tempList);
+  // Add userResponses to the data section
+  tempList.data.push(userResponses);
   // Add back to localStorage with the new values added
-  localStorage.setItem('inputs', tempList);
+  localStorage.setItem('data', JSON.stringify(tempList));
 }
 
 // Display method runs to display the information on screen
 // Parameters: list of data to use (one day), whether or not to replace all the window or just to append (boolean)
 function display(data, replace) {
-  // I'm collecting all the checkboxes in one list, and then I'll access this list
-  // when making the data structure.
-  /*var checkboxes = document.getElementsByName("breakfast"); // the elements clear later so it's necessary to create a separate list
-  // Here is the JSON that I'll be using.
-  var userResponses = {
-    'name': document.getElementById('name').value,
-    'ateBreakfast': document.getElementById('yesBreakfast').checked,
-    'frequentlyEats': [
-      ["eggs", checkboxes[0].checked], 
-      ["pancakes", checkboxes[1].checked], 
-      ["waffles", checkboxes[2].checked], 
-      ["cereal", checkboxes[3].checked], 
-      ["fruit", checkboxes[4].checked], 
-      ["something else", checkboxes[5].checked] 
-    ],
-    'date': Date.now()
-  };*/
+  // Debugging: making sure that the data is corerct
   console.log(data);
   if (replace) {
     myDiv.innerHTML = "\n"; // replace the div tag's contents
@@ -78,5 +61,9 @@ function display(data, replace) {
     if (food[1]) {
       myDiv.innerHTML += "\t\t\t<li> " + data.name + " eats " + food[0] + " for breakfast sometimes. </li>\n";
     }
+  }
+  // TODO add "see past answers" when there was a replacement
+  if (replace) {
+    // TODO add button, call "add past responses" 
   }
 }
